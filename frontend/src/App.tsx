eb69e4122b21dffc9749a5d1b085c7362beabf40
damianosdam4 <86740950+damianosdam4@users.noreply.github.com>
2022-05-05 22:11:28 +0300

    navbar- footer

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,18 +7,17 @@ import Home from './components/Home'
 import Layout from './components/Layout'
 import { RouteProps } from "react-router-dom";
 import '@fortawesome/fontawesome-free/css/all.min.css';
-import 'bootstrap-css-only/css/bootstrap.min.css';
-import 'mdbreact/dist/css/mdb.css';
+import './css/lux/bootstrap.min.css';
+
 import Register from './components/Register';
 
 function App() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path='/' element={<Home />} />
                 <Route path='/login' element={<AuthGuard loginGuard={true}>< Login /></AuthGuard>} />
                 <Route path='/register' element={<AuthGuard loginGuard={true}>< Register /></AuthGuard>} />
-                <Route path='/' element={<AuthGuard><Layout><Home /></Layout></AuthGuard>} />
+                <Route path='/' element={<Layout><Home /></Layout>} />
                 <Route path='*' element={<Navigate to="/" replace />} />
             </Routes>
         </BrowserRouter>
