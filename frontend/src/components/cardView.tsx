3e1a6378ba98eaeeb051fc8332169cda01aadd32
giannisgeo344 <giannisgeo344@gmail.com>
2022-05-07 14:26:06 +0300

    created cardView

---
+++ frontend/src/components/cardView.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import '../css/lux/bootstrap.min.css'
+
+const cardView: React.FC = () => {
+    return (
+        <React.Fragment>
+
+        </React.Fragment>
+    );
+}
+
+export default cardView;
