d6ae1dfd22abef0a82f19df3cf7258c6df331897
giannisgeo344 <giannisgeo344@gmail.com>
2022-05-07 14:50:42 +0300

    cardView

--- frontend/src/components/VerticalCard.tsx
+++ frontend/src/components/VerticalCard.tsx
@@ -1,14 +1,27 @@
 import Card from "react-bootstrap/Card";
-import React, { Component, useState } from "react";
+import axios from "axios";
+import React, { Component, useEffect, useState } from "react";
 import { Button, Row } from "react-bootstrap";
 import '../css/lux/bootstrap.min.css';
 import dildoImg from '../images/dildo.png';
+import { WALLET_BACKEND } from '../config';
 
-const VerticalCard: React.FC = () => {
+interface VerticalCardProps {
+    productId: number;
+}
+
+const VerticalCard: React.FC<VerticalCardProps> = ({ productId }) => {
+
+    useEffect(() => {
+        await axios.get(WALLET_BACKEND + "/product/" + +productId)
+    }, [])
+
+
+
     let MAX_LENGTH: number = 20;
     const [showMore, setShowMore] = useState(false);
     const text: string = "Large realistic penis dummy with vibration. Super elastic texture and intense relief that fascinates! With strong suction cup in the base for hands-free situations and adjustable vibrations with a dimmer. Material: TPE / ABS / PVC. Total length 26cm, penetrating length 21.5cm, diameter 5cm. We operate 2 AA batteries (not included).";
-    const price: number = 99.99;
+    const price: number = productId;
     const currency: string = "EUR";
     let currencySymbol: string = '';
     if (currency == "EUR") {
