66aaf265bcf353feb1e430ec6f979d1dc3ad7134
giannisgeo344 <giannisgeo344@gmail.com>
2022-05-09 11:56:43 +0300

    showCase

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -14,6 +14,7 @@ import Register from './components/Register';
 import Category from './components/Category';
 import CardView from './components/cardView';
 import AddProductItem from './components/AddProductItem';
+import ProductShowCase from './components/ProductShowCase';
 
 function App() {
     return (
@@ -25,9 +26,9 @@ function App() {
                 {/* <Route path='/' element={<AuthGuard><Layout><Home /></Layout></AuthGuard>} /> */}
                 <Route path='*' element={<Navigate to="/" replace />} />
                 <Route path='/VerticalCard' element={<Layout><CardView /></Layout>} />
-                <Route path='/category/:cat' element={<Layout><Category/></Layout>} />
-
-                <Route path='/addproduct' element={<AuthGuard><Layout><AddProductItem/></Layout></AuthGuard>} />
+                <Route path='/category/:cat' element={<Layout><Category /></Layout>} />
+                <Route path='/ProductShowCase' element={<Layout><ProductShowCase /></Layout>} />
+                <Route path='/addproduct' element={<AuthGuard><Layout><AddProductItem /></Layout></AuthGuard>} />
 
                 {/* <Route path='/addproduct' element={<Layout>   <Category/>  </Layout>} /> */}
             </Routes>
