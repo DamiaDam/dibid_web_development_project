1da852478112f7c979803ec82f96011fccd6de41
giannisgeo344 <giannisgeo344@gmail.com>
2022-05-09 19:13:08 +0300

    required fields

--- frontend/src/components/Register.tsx
+++ frontend/src/components/Register.tsx
@@ -93,35 +93,35 @@ const Register: React.FC = () => {
           <FormGroup >
             <Form.Label className="form-label mt-4"><h3>Register</h3></Form.Label>
             <Form.Floating className="mb-3">
-              <input onKeyPress={(e) => e.key === 'Enter' && register()} type="text" ref={username} className="form-control" id="username" placeholder="Username"></input>
+              <input onKeyPress={(e) => e.key === 'Enter' && register()} type="text" ref={username} className="form-control" id="username" placeholder="Username" required />
               <label htmlFor="username">Username</label>
             </Form.Floating>
             <Form.Floating className="mb-3">
-              <input onKeyPress={(e) => e.key === 'Enter' && register()} type="password" ref={password} className="form-control" id="password" placeholder="Password"></input>
+              <input onKeyPress={(e) => e.key === 'Enter' && register()} type="password" ref={password} className="form-control" id="password" placeholder="Password" required />
               <label htmlFor="password">Password</label>
             </Form.Floating>
             <Form.Floating className="mb-3">
-              <input onKeyPress={(e) => e.key === 'Enter' && register()} type="password" ref={rptpassword} className="form-control" id="confirm-password" placeholder="Password"></input>
+              <input onKeyPress={(e) => e.key === 'Enter' && register()} type="password" ref={rptpassword} className="form-control" id="confirm-password" placeholder="Password" required />
               <label htmlFor="confirm-password">Confirm Password</label>
             </Form.Floating>
             <Form.Floating className="mb-3">
-              <input onKeyPress={(e) => e.key === 'Enter' && register()} type="text" ref={name} className="form-control" id="name" placeholder="Name"></input>
+              <input onKeyPress={(e) => e.key === 'Enter' && register()} type="text" ref={name} className="form-control" id="name" placeholder="Name" required />
               <label htmlFor="name">Name</label>
             </Form.Floating>
             <Form.Floating className="mb-3">
-              <input onKeyPress={(e) => e.key === 'Enter' && register()} type="text" ref={surname} className="form-control" id="surname" placeholder="Surname"></input>
+              <input onKeyPress={(e) => e.key === 'Enter' && register()} type="text" ref={surname} className="form-control" id="surname" placeholder="Surname" required />
               <label htmlFor="surname">Surname</label>
             </Form.Floating>
             <Form.Floating className="mb-3">
-              <input onKeyPress={(e) => e.key === 'Enter' && register()} type="email" ref={email} className="form-control" id="email" placeholder="name@example.com"></input>
+              <input onKeyPress={(e) => e.key === 'Enter' && register()} type="email" ref={email} className="form-control" id="email" placeholder="name@example.com" required />
               <label htmlFor="email">Email address</label>
             </Form.Floating>
             <Form.Floating className="mb-3">
-              <input onKeyPress={(e) => e.key === 'Enter' && register()} type="text" ref={phone} className="form-control" id="phone" placeholder=""></input>
+              <input onKeyPress={(e) => e.key === 'Enter' && register()} type="text" ref={phone} className="form-control" id="phone" placeholder="" />
               <label htmlFor="phone">Phone Number</label>
             </Form.Floating>
             <Form.Floating className="mb-3">
-              <input onKeyPress={(e) => e.key === 'Enter' && register()} type="text" ref={tin} className="form-control" id="tin" placeholder=""></input>
+              <input onKeyPress={(e) => e.key === 'Enter' && register()} type="text" ref={tin} className="form-control" id="tin" placeholder="" />
               <label htmlFor="tin">TIN</label>
             </Form.Floating>
             <Form.Floating className="mb-3">
