36f6f9c1d070f7345edf476d13e27041c6ddcae1
giannisgeo344 <giannisgeo344@gmail.com>
2022-08-27 01:20:40 +0300

    messageboard

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -20,6 +20,7 @@ import ProductView from './components/ProductView';
 import MyProducts from './components/MyProducts';
 import PendingValidation from './components/PendingValidation';
 import ViewCurrentAuctions from './components/ViewCurrentAuctions';
+import MessageBoard from './components/Messageboard';
 
 
 function App() {
@@ -35,13 +36,14 @@ function App() {
                 <Route path='/myproducts' element={<AuthGuard ><Layout><MyProducts /></Layout></AuthGuard>} />
                 <Route path='/addproduct' element={<AuthGuard ><Layout><AddProductItem /></Layout></AuthGuard>} />
                 <Route path='/users/user/:usr' element={<AuthGuard adminGuard={true}><Layout><UserView /></Layout></AuthGuard>} />
-                <Route path='/users/:userType' element={<AuthGuard adminGuard={true}><Layout><ManageUsers /></Layout></AuthGuard>} />
+                <Route path='/users/:userType' element={<AuthGuard adminGuard={false}><Layout><ManageUsers /></Layout></AuthGuard>} />
                 <Route path='/product/:productId' element={<Layout><ProductView /></Layout>} />
                 <Route path='/pending' element={<Layout><PendingValidation /></Layout>} />
                 <Route path='/auctions' element={<Layout><ViewCurrentAuctions /></Layout>} />
+                <Route path='/message' element={<Layout><MessageBoard /></Layout>} />
                 <Route path='*' element={<Navigate to="/" replace />} />
             </Routes>
-        </BrowserRouter>
+        </BrowserRouter >
     );
 }
 
---
+++ frontend/src/components/Messageboard.tsx
@@ -0,0 +1,99 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { Col } from "react-bootstrap";
+import { useNavigate, useParams } from "react-router-dom";
+import { WALLET_BACKEND } from "../config";
+import { UserInfoDTO } from "../interfaces";
+
+interface UserInfoList {
+    users: UserInfoDTO[]
+}
+
+interface UserCardInterface {
+    user: UserInfoDTO;
+}
+
+const UserCard: React.FC<UserCardInterface> = ({ user }) => {
+
+    return (
+        <React.Fragment>
+            <a
+                className="list-group-item list-group-item-action py-2 ripple"
+                aria-current="true"
+            >
+                <span>{user.username}</span>
+            </a>
+        </React.Fragment>
+    );
+}
+
+const MessengerList: React.FC<UserInfoList> = ({ users }) => {
+
+    const navigate = useNavigate();
+
+    const renderList = (): JSX.Element[] => {
+        console.log(users);
+        return users.map((user: UserInfoDTO, index: any) => {
+            return (
+                <tr key={index} id={index} onClick={() => navigate('/users/user/' + user.username)}>
+                    <UserCard user={user} />
+                </tr>
+            );
+        });
+    }
+
+    return (
+        <React.Fragment>
+            {renderList()}
+        </React.Fragment>
+    );
+
+}
+
+const MessageBoard: React.FC = () => {
+
+
+    //Get all messengers
+    const params = useParams();  // useState for UserInfo items
+    const [users, setUsers] = useState<UserInfoDTO[]>([]);
+
+    const axiosgetURL: string = `${WALLET_BACKEND}/users/${params.userType}`
+    // useEffect to get all user info then set
+    useEffect(() => {
+
+        const getAllUsers = async () => {
+            const res = await axios.get(axiosgetURL,
+                {
+                    headers: {
+                        Authorization: `Bearer ${localStorage.getItem('apptoken')}`
+                    }
+                }
+            );
+            console.log('res: ', res);
+            setUsers(res.data);
+        }
+
+        getAllUsers();
+    }, [])
+
+
+    return (
+        <React.Fragment>
+            <Col md={2}>
+                <nav
+                    id="sidebarMenu"
+                    className="collapse d-lg-block sidebar collapse bg-white"
+                >
+                    <div className="position-sticky">
+                        <div className="list-group list-group-flush mx-3 mt-4">
+                            <MessengerList users={users} />
+                        </div>
+                    </div>
+                </nav>
+            </Col>
+        </React.Fragment>
+    );
+
+}
+
+export default MessageBoard;
